3f7f5ebc794e895ff0c81355b2238f3f05bf37ab
inuiyuto <94748712+inuiyuto@users.noreply.github.com>
2026-03-08 05:08:20 +0000

    Deploying to gh-pages from @ inuiyuto/inuiyuto.github.io@8ba426a0dc779714dcf414558680d16578def3fa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -95,7 +95,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=bsu9grEAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
